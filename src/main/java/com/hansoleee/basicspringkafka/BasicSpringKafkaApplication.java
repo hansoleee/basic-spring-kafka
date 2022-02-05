929a5f3b45181a210cd0bce5e412ce51e6749678
hansoleee <leehansol.kr@gmail.com>
2022-02-06 00:36:34 +0900

    first commit

---
+++ src/main/java/com/hansoleee/basicspringkafka/BasicSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.hansoleee.basicspringkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringKafkaApplication.class, args);
+    }
+
+}
